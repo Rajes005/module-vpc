8f959ebfdd3e9a8aa1d23215ff6f79ea31da560c
Rajes005 <37709185+Rajes005@users.noreply.github.com>
2023-07-12 17:58:16 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,8 +1,8 @@
 output "vpc_id" {
   value = aws_vpc.vpc.id
 }
-output "internet_gateway_id" {
-  value = aws_internet_gateway.internet_gateway.id
+output "internet_gateway" {
+  value = aws_internet_gateway.internet_gateway
 }
 output "public_subnet_az1_id" {
   value = aws_subnet.public_subnet_az1.id
